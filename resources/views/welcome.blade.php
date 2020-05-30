54d32bbf61160da302f7b5a288b43ebce1c80b19
haruka.nakasaka <haruka128050@gmail.com>
2020-05-30 13:03:30 +0000

    modify card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,22 +38,22 @@
                         <div class="my-tape"></div>
                         <li class="media mb-3">
                         <div class="media-left">
-                            <p class="title-space mb-0">{{ $log->product_name }}</p>
+                            <p class="title-space mt-3 mb-1">{{ $log->product_name }}</p>
                             @if ($log->myfile)
-                            <img src="{{ $log->myfile }}" width="180rem" height="180rem">
+                            <img src="{{ $log->myfile }}" width="190rem" height="190rem">
                             @endif
                         </div>
-                        <div class="media-body ml-4">
+                        <div class="media-body ml-5">
                             <div>
-                                <p class="mb-3">{{ $log->title }}</p>
+                                <p class="title mb-2">{{ $log->title }}</p>
                             </div>
                             <div>
                                 <p class="comment-space mb-0">{!! nl2br(e($log->comment)) !!}</p>
                             </div>
                             
                             <div>
-                                {{ $log->user->name }} <span class="text-muted ml-3">{{ $log->created_at }}に投稿</span>
-                                <div class="form-inline float-right">   
+                                <p class="post">{{ $log->user->name }} <span class="text-muted ml-3">{{ $log->created_at }}に投稿</span></p>
+                                <div class="form-inline float-right card-button">   
                                 @if (Auth::id() == $log->user_id)
                                 {!! Form::open(['route' => ['logs.destroy', $log->id], 'method' => 'delete']) !!}
                                     {!! Form::submit('削除', ['class' => 'btn btn-light rounded-pill btn-rem-6 mr-2 negative']) !!}
